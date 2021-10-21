eaee19e5cc13d0abeb87803679892cb11cda783f
Opilane TTHK <opilane@tthk.ee>
2021-10-21 09:43:30 +0300

    initial commit

---
+++ FunctionsWithReturns/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FunctionsWithReturns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] food = { "Kananagitsad", "burger", "friikartulid", "salat" };
+            string[] drinks = { "cocacola", "fanta", "sprite", "tavaline vesi" };
+            string[] movie = { "The birds", "Friday 13", "John Wick", "Shrek", "Pirates Of The Carribean"};
+
+            //DisplayRandomFromArray(food);
+            //DisplayRandomFromArray(drinks)
+            string randomSnack = PickRandomFromArray(food);
+            string randomDrink = PickRandomFromArray(drinks);
+            string randomMovie = PickRandomFromArray(movie);
+
+            Console.WriteLine($"Computer picked {randomSnack} and {randomDrink}. for a {randomMovie} night.");
+        }
+        private static string PickRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            string randomElement = someArray[randomIndex];
+
+            return randomElement;
+        }
+        
+        
+        private static void DisplayRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            Console.WriteLine($"computer has chosen {someArray[randomIndex]}");
+        }
+
+
+
+    }
+}
---
+++ Hero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero, villain;
+            hero = RandomHero();
+            Console.WriteLine($"Random hero: {hero}");
+        }
+
+        private static string RandomHero()
+        {
+            string[] heroes = { "Batman", "Spongebob", "Spiderman", "Patric", "Lara Croft" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, heroes.Length);
+
+            string randomHero = heroes[randomIndex];
+            return randomHero;
+        }
+
+        private static string RandomVillain()
+
+    }
+}
